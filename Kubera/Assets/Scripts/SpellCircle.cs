f8efea59536f8872871dd834dbb846e54534eb78
hydrater <liquid_assassin@hotmail.com>
2016-12-28 17:21:34 +0800

    Memory optmisation

--- Kubera/Assets/Scripts/SpellCircle.cs
+++ Kubera/Assets/Scripts/SpellCircle.cs
@@ -17,16 +17,16 @@ public class SpellCircle : MonoBehaviour {
 	IEnumerator DeathTimer()
 	{
 		Vector3 final = new Vector3(0.8f,0.8f,0.8f);
-		float progress = 0;
+		float t = 0;
      
-	    while(progress <= 1){
-			transform.localScale = Vector3.Lerp(transform.localScale, final, progress);
-	        progress += Time.deltaTime;
+	    while(t <= 1){
+			transform.localScale = Vector3.Lerp(transform.localScale, final, t);
+	        t += Time.deltaTime;
 	        yield return null;
 	    }
 		transform.localScale = final;
 		yield return new WaitForSeconds(0.5f);
-		for (float t = 0.0f; t < 1; t += Time.deltaTime/2)
+		for (t = 0.0f; t < 1; t += Time.deltaTime/2)
 	    {
 	        Color newColor = new Color(1, 1, 1, Mathf.Lerp(1,0,t));
 	        transform.GetComponent<Renderer>().material.color = newColor;
