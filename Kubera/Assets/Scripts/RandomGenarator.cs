e883e4c839fe293df8ba4af8b3cb87d0cf2c424e
FantasyLance <fantasylance@gmail.com>
2016-12-31 16:28:28 +0800

    randomgenarator scrit

---
+++ Kubera/Assets/Scripts/RandomGenarator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomGenarator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int n = Random.Range(0,10);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
