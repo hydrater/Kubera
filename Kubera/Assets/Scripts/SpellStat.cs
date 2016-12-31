4bf0e89506d99f336614154e5ac851631439355b
hydrater <liquid_assassin@hotmail.com>
2016-12-31 18:09:23 +0800

    Added prefix aesthetic integration script as comment

--- Kubera/Assets/Scripts/SpellStat.cs
+++ Kubera/Assets/Scripts/SpellStat.cs
@@ -5,10 +5,18 @@ public class SpellStat : MonoBehaviour {
 	public double damage;
 	public char type, element, effect;
 	public float duration, speed;
+	public Material mat;
+
+	public GameObject childMaterial;
 
 	public SpellStat()
 	{
 		
+	}
+
+	void Start()
+	{
+		//childMaterial.GetComponent<Renderer>().material = mat;
 	}
 
 }
